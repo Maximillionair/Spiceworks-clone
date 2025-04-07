e1c97d624cc761fa276d40c0ba91c8ea56a1ad05
Maximillionair <maxmillian.heim@outlook.com>
2025-04-07 12:39:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {connectDB} = require('./config/db');
 // Load env vars
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
   connectDB()
