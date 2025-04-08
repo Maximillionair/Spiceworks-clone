e2c31e731df81ef01489deb3b932a36b4d10b653
Maximillionair <maxmillian.heim@outlook.com>
2025-04-08 10:39:05 +0200

    Update ticketRoutes.js

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,38 +1,28 @@
 const express = require('express');
-const {
-  getTickets,
-  getTicket,
-  createTicket,
-  updateTicket,
-  getTicketStats
+const { 
+  createTicket, 
+  getTickets, 
+  getTicket, 
+  updateTicket, 
+  getTicketStats 
 } = require('../controllers/ticketController');
-
-const { getTicketComments, addComment } = require('../controllers/commentController');
-
 const { protect, authorize } = require('../middleware/authmiddleware');
-const { apiLimiter } = require('../middleware/rateLimitMiddleware');
 
 const router = express.Router();
 
-// Apply protection to all routes
+// Apply authentication middleware to all routes
 router.use(protect);
-router.use(apiLimiter);
 
-// Ticket routes
+// Routes for tickets
 router.route('/')
-  .get(getTickets)
-  .post(createTicket);
+  .post(createTicket)
+  .get(getTickets);
 
 router.route('/stats')
   .get(authorize('admin'), getTicketStats);
 
-router.route('/:ticketId')  // Ensure ':ticketId' matches the parameter used in your controllers
+router.route('/:id')
   .get(getTicket)
   .put(authorize('admin'), updateTicket);
-
-// Comment routes
-router.route('/:ticketId/comments')  // Ensure ':ticketId' matches the parameter used in your controllers
-  .get(getTicketComments)
-  .post(addComment);
 
 module.exports = router;
