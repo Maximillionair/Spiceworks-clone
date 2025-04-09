76faf71afe19b94ea20df599236bfd8c52c919ec
Maximillionair <maxmillian.heim@outlook.com>
2025-04-09 12:56:02 +0200

    Update viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -33,7 +33,7 @@ const setTokenCookie = (user, res) => {
   res.cookie('token', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     sameSite: 'strict'
   });
 };
